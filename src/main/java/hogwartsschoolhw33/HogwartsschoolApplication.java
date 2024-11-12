f7abd77ec76d5e7c60a84e3bb451a9549bb39567
Alex1445085 <n1445085@yandex.ru>
2024-11-12 15:15:05 +0300

    initial commit

---
+++ src/main/java/hogwartsschoolhw33/HogwartsschoolApplication.java
@@ -0,0 +1,13 @@
+package hogwartsschoolhw33;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwartsschoolApplication.class, args);
+	}
+
+}
